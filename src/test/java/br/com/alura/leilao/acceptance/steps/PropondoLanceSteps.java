6cd43eb4f838c65c6e7e72eb01a35ccbce9829d2
Tiago Silva <tiagobsb84@gmail.com>
2022-04-16 16:02:28 -0300

    Novo cenario

--- src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
+++ src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
@@ -15,16 +15,18 @@ public class PropondoLanceSteps {
 
 	private Lance lance;
 	private Leilao leilao;
+	private Lance lance15;
+	private Lance lance10;
 	
 	@Dado("Dado um lance valido")
 	public void dado_um_lance_valido() {
 	  Usuario usuario = new Usuario("fulano");
 	  lance = new Lance(usuario, BigDecimal.TEN);
+	  leilao = new Leilao("Tablet XPTO");
 	}
 
-	@Quando("Quando propoe um lance")
+	@Quando("Quando propoe um leilao")
 	public void quando_propoe_um_lance() {
-		leilao = new Leilao("Tablet XPTO");
 		leilao.propoe(lance);
 	}
 	
@@ -32,5 +34,27 @@ public class PropondoLanceSteps {
 	public void entao_o_lance_eh_aceito() {
 		Assert.assertEquals(1, leilao.getLances().size());
 		Assert.assertEquals(BigDecimal.TEN, leilao.getLances().get(0).getValor());
+	}
+	
+	@Dado("Dados varios lances validos")
+	public void dados_varios_lances_validos() {
+		Usuario usuario = new Usuario("fulano");
+		Usuario usuario1 = new Usuario("beltrano");
+		lance10 = new Lance(usuario, BigDecimal.TEN);
+		lance15 = new Lance(usuario1, new BigDecimal("15.0"));
+		leilao = new Leilao("Tablet XPTO");
+	}
+
+	@Quando("Quando propoe varios lances ao leilao")
+	public void quando_propoe_varios_lances_ao_leilao() {
+		leilao.propoe(lance10);
+		leilao.propoe(lance15);
+	}
+	
+	@Entao("Entao os lances sao aceitos")
+	public void entao_os_lances_sao_aceitos() {
+		Assert.assertEquals(2, leilao.getLances().size());
+		Assert.assertEquals(BigDecimal.TEN, leilao.getLances().get(0).getValor());
+		Assert.assertEquals(new BigDecimal("15.0"), leilao.getLances().get(1).getValor());
 	}
 }
--- src/test/resources/features/propondo_lances.feature
+++ src/test/resources/features/propondo_lances.feature
@@ -1,8 +1,13 @@
 # language: pt
 
-Funcionalidade: Propondo lances
+Funcionalidade: Propondo lances ao leilao
 
 Cenario: Propondo um unico lance valido
 Dado Dado um lance valido
-Quando Quando propoe um lance
+Quando Quando propoe um leilao
 Entao Entao o lance eh aceito
+
+Cenario: Propondo varios lances validos
+Dado Dados varios lances validos
+Quando Quando propoe varios lances ao leilao
+Entao Entao os lances sao aceitos
